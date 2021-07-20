46f8e7ff50ed0e126b06fb3036f1eb18a44055c6
David <sky80420@hotmail.com>
2021-07-20 22:39:16 +0800

    Initial commit

---
+++ App.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script lang="ts">
+    export default {
+        data() {
+            return {
+                msg: 'Hello, world!',
+            }
+        },
+        methods: {
+            init() {
+                const x = 'Hello';
+                this.msg = ''
+            }
+        }
+        
+    }
+</script>
+
+<style lang="sass" scoped>
+
+</style>
